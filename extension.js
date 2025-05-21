8aa99b7ebdfab1ffc1e5591a1e483eefe024b879
demopath <28894906+demopath@users.noreply.github.com>
2025-05-22 04:37:45 +0800

    删除配置

--- extension.js
+++ extension.js
@@ -2,22 +2,11 @@ const vscode = require('vscode');
 
 function activate(context) {
     let buttons = [];
-    
     function updateTaskButtons() {
         buttons.forEach(btn => btn.dispose());
         const tasks = vscode.workspace.getConfiguration('tasks').get('tasks') || [];
-        const displayCondition = vscode.workspace.getConfiguration('tasks').get('DisplayCondition') || 'ShowWithIcon';
-        
         buttons = tasks
-            .filter(task => task.label)
-            .filter(task => {
-                switch(displayCondition) {
-                    case 'ShowAll': return true;
-                    case 'HideAll': return false;
-                    case 'ShowWithIcon': return !!(task.icon?.id);
-                    default: return false;
-                }
-            })
+            .filter(task => task.label && task.icon)
             .map(task => {
                 const btn = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 100);
                 btn.text = task.icon?.id ? `$(${task.icon.id}) ${task.label}` : task.label;
@@ -28,9 +17,7 @@ function activate(context) {
                 return btn;
             });
     }
-
     updateTaskButtons();
-    
     context.subscriptions.push(
         vscode.workspace.onDidChangeConfiguration(e => {
             e.affectsConfiguration('tasks') && updateTaskButtons();
